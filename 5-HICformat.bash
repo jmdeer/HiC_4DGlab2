f36fd8b9f3bac04a28d009108845ce83a0e1e51a
jmdeer <79147262+jmdeer@users.noreply.github.com>
2021-11-10 15:51:08 +0800

    Update 5-HICformat.bash

--- 5-HICformat.bash
+++ 5-HICformat.bash
@@ -1,5 +1,5 @@
 #.allValidPairs 2 .hic
-nohup /together_sdb/zhoujiaqi/software/HiC-Pro-master/bin/utils/hicpro2juicebox.sh -i H-naive.allValidPairs -g /together_sdb/zhoujiaqi/software/HiC-Pro-master/annotation/chrom_hg38.sizes -j /together_sdb/zhoujiaqi/software/juicer_tools_1.21.01.jar &
+nohup /home/yuanzan/install/HiC-Pro/HiC-Pro_3.1.0/bin/utils/hicpro2juicebox.sh -i CT.allValidPairs -g /home/project/alignIndex/Ensembl.Mus_musculus.GRCm38.102/bowtie2/chrom_Ensembl.Mus_musculus.GRCm38.102.sizes -j /home/project/jimengdie/BL-HiC/juicer_tools_1.22.01.jar &
 
 #.allValidPairs 2 .h5
 hicConvertFormat -m /together_sdb/zhoujiaqi/0609HIC/ANNO_XS01KF2021030087_PM-XS01KF2021030087-01_AHG3JGCCX2_2021-06-06_14-50-39/Clean-result/hic_results/matrix/H-IgD/iced/40000/H-IgD_40000_iced.matrix --bedFileHicpro /together_sdb/zhoujiaqi/0609HIC/ANNO_XS01KF2021030087_PM-XS01KF2021030087-01_AHG3JGCCX2_2021-06-06_14-50-39/Clean-result/hic_results/matrix/H-IgD/raw/40000/H-IgD_40000_abs.bed --inputFormat hicpro --outputFormat h5 -o H-IgD.matrix.h5
